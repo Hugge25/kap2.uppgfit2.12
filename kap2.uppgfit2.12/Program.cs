c4233c069b03574d9d2e4e3f25804c7ef7cd3fcb
Hugge25 <hugo.paulsson@hotmail.com>
2022-09-09 20:49:18 +0200

    Add project files.

---
+++ kap2.uppgfit2.12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace kap2.uppgft2._12
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur många kilometer ska du åka?");
+            string km = Console.ReadLine();
+
+            Console.WriteLine("Hur många dagar vill du hyra bilen?");
+            string dagar = Console.ReadLine();
+            
+            int Dagar = int.Parse(dagar);
+            int Km = int.Parse(km);
+
+            Console.WriteLine(300+Km+(Dagar*500)-500);
+        }
+    }
+}
